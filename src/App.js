d060f99162b235e259db4e1ceb95395afa8ae0f4
Mitch D Lincoln <mitchdlincoln@gmail.com>
2022-02-11 19:51:16 +0100

    :sparkles: Aggiunto un nuovo elemnto per testare il tema

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import { Button } from '@windmill/react-ui'
+import { Alert } from '@windmill/react-ui'
+
 
 function App() {
-  return <Button>Hi there!</Button>
+
+  return (
+  <>
+    <Button>Hi there!</Button>
+    <Alert>Lorem ipsum dolor sit</Alert>
+  </>
+  )
+  
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { Windmill } from '@windmill/react-ui'
 
 ReactDOM.render(
-  <Windmill dark>
+  <Windmill usePreferences>
     <App />
   </Windmill>,
   document.getElementById('root')
