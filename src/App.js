803ac7149db21cd53db918743fac1981a8fb850b
Mitch D Lincoln <mitchdlincoln@gmail.com>
2022-02-18 14:13:23 +0100

    :construction: Aggiornato Creato le rotte per le pagine, al momento tutto fermo pero

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { Button } from '@windmill/react-ui'
-import { Alert } from '@windmill/react-ui'
+import React, { lazy } from 'react'
+import { Router, Switch, Route, Redirect } from 'react-router-dom'
 
+const Layout = lazy(() => import('./containers/Layout'))
 
 function App() {
 
   return (
   <>
-    <Button>Hi there!</Button>
-    <Alert>Lorem ipsum dolor sit</Alert>
+  <Router>
+    <Switch>
+{/*       <Route path="/login" component={Login} />
+      <Route path="/create-account" component={CreateAccount} />
+      <Route path="/forgot-password" component={ForgotPassword} /> */}
+
+      <Route path="/app" component={Layout} />
+
+      <Redirect exact from="/" to="/app" />
+    </Switch>
+  </Router>
+
   </>
   )
   
